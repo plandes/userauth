1811b2d33e25f972e8a4019adbb79fd25943dc39
Paul Landes <landes@mailc.net>
2019-12-27 21:24:11 -0600

    add command line test

--- src/main/java/com/zensols/sys/userauth/UnixUserManager.java
+++ src/main/java/com/zensols/sys/userauth/UnixUserManager.java
@@ -8,6 +8,10 @@ public class UnixUserManager {
 
     private String pwauthPath;
 
+    public UnixUserManager() {
+	this("/usr/bin/pwauth");
+    }
+
     public UnixUserManager(String pwauthPath) {
 	this.pwauthPath = pwauthPath;
     }
@@ -18,5 +22,26 @@ public class UnixUserManager {
 
     public UnixUser createUser(String userName) {
 	return new UnixUser(this, userName);
+    }
+
+    public static void main(String[] args) throws Exception {
+	UnixUser usr = null;
+	String password = null;
+	if (args.length == 2) {
+	    UnixUserManager mng = new UnixUserManager();
+	    usr = mng.createUser(args[0]);
+	    password = args[1];
+	} else if (args.length == 3) {
+	    UnixUserManager mng = new UnixUserManager(args[2]);
+	    usr = mng.createUser(args[0]);
+	    password = args[1];
+	} else {
+	    System.err.println("usage: java ... <user> <password> [pwauth path]");
+	    System.exit(1);
+	}
+	if (log.isDebugEnabled()) {
+	    log.debug("user: " + usr);
+	}
+	System.out.println(usr.getStatusForPassword(password));
     }
 }
