212b45c46a65deaf4cbde7bfd2c8ea45f08136d2
Paul Landes <landes@mailc.net>
2019-12-28 11:14:35 -0600

    test semantics

--- src/test/java/com/zensols/unix/userauth/UserManagerTest.java
+++ src/test/java/com/zensols/unix/userauth/UserManagerTest.java
@@ -5,14 +5,19 @@ import java.io.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserManagerTest {
     private static final Logger log = LoggerFactory.getLogger(UserManagerTest.class);
     private static UserManager mng;
 
-    public UserManagerTest() {
+    @Before
+    public void setUp() {
 	this.mng = new UserManager();
 	this.mng.overridePath("pwauth", "src/test/python/pwauth.py");
 	this.mng.overridePath("getent", "src/test/python/getent.py");
@@ -23,8 +28,8 @@ public class UserManagerTest {
 	Command bin = this.mng.getCommand("ls");
 	log.debug("found: " + bin);
 	File path = bin.find();
-	Assert.assertNotNull(path);
-	Assert.assertTrue(path.getPath().endsWith("ls"));
+	assertNotNull(path);
+	assertTrue(path.getPath().endsWith("ls"));
     }
 
     @Test(expected = SystemException.class)
@@ -42,14 +47,14 @@ public class UserManagerTest {
 	}
 
 	UserAuthStatus status = usr.getStatusForPassword("wrongpasswd");
-	Assert.assertEquals(UserAuthStatus.INVALID, status);
+	assertEquals(UserAuthStatus.INVALID, status);
 
 	status = usr.getStatusForPassword("pass123");
-	Assert.assertEquals(UserAuthStatus.OK, status);
+	assertEquals(UserAuthStatus.OK, status);
 
 	usr = mng.createUser("nosuchuser");
 	status = usr.getStatusForPassword("pass123");
-	Assert.assertEquals(UserAuthStatus.UNKNOWN, status);
+	assertEquals(UserAuthStatus.UNKNOWN, status);
     }
 
     @Test
@@ -60,26 +65,25 @@ public class UserManagerTest {
 	}
 
 	UserAuthStatus status = usr.getStatusForPassword("wrongpasswd");
-	Assert.assertFalse(usr.isAuthorized("wrongpasswd"));
-
-	Assert.assertTrue(usr.isAuthorized("pass123"));
+	assertFalse(usr.isAuthorized("wrongpasswd"));
+	assertTrue(usr.isAuthorized("pass123"));
 
 	usr = mng.createUser("jane");
-	Assert.assertFalse(usr.isAuthorized("pass123"));
-	Assert.assertTrue(usr.isAuthorized("changeit"));
+	assertFalse(usr.isAuthorized("pass123"));
+	assertTrue(usr.isAuthorized("changeit"));
 
 	usr = mng.createUser("goofy");
-	Assert.assertFalse(usr.isAuthorized("pass123"));
+	assertFalse(usr.isAuthorized("pass123"));
     }
 
     @Test
     public void testUserInfo() throws Exception {
 	User usr = mng.createUser("bob");
-	Assert.assertEquals(701, usr.getUserId());
-	Assert.assertEquals("Bob Copymeister", usr.getFullName());
+	assertEquals(701, usr.getUserId());
+	assertEquals("Bob Copymeister", usr.getFullName());
 
 	usr = mng.createUser("jane");
-	Assert.assertEquals(702, usr.getUserId());
-	Assert.assertEquals("Jane Smith", usr.getFullName());
+	assertEquals(702, usr.getUserId());
+	assertEquals("Jane Smith", usr.getFullName());
     }
 }
